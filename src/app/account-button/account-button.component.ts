0bafd9e205c0d9e1d0dbacd55f242580b92160c8
Lukasz Kostrzewa <lukasz.kostrzewa@sabre.com>
2017-06-21 19:04:26 +0200

    app: Add account md-fab button cont.

---
+++ src/app/account-button/account-button.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-account-button',
+  templateUrl: './account-button.component.html',
+  styleUrls: ['./account-button.component.css']
+})
+export class AccountButtonComponent {
+
+  initials: string = "LK";
+
+}
