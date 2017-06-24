34c0ce34843734b893e0dc7e0bcfbd59435b3380
Lukasz Kostrzewa <lukasz.webmaster@gmail.com>
2017-06-24 15:20:26 +0200

    app: Replace Linkurious.js with Cytoscape.js

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -8,9 +8,9 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/
 export class GraphComponent implements OnInit, AfterViewInit {
 
   @ViewChild('graphContainer') container: ElementRef;
-  private graph = {nodes: [], edges: []};
 
   private editMode: boolean;
+  private cy: Cy.Instance;
 
   setEditMode(value) {
     this.editMode = value;
@@ -21,32 +21,19 @@ export class GraphComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    let N = 10, E = 40;
-    for (let i = 0; i < N; i++) {
-      this.graph.nodes.push({
-        id: 'n' + i,
-        label: 'Node ' + i,
-        x: Math.random(),
-        y: Math.random(),
-        size: Math.random(),
-        color: '#666'
-      });
-    }
-    for (let i = 0; i < E; i++) {
-      this.graph.edges.push({
-        id: 'e' + i,
-        source: 'n' + (Math.random() * N | 0),
-        target: 'n' + (Math.random() * N | 0),
-        size: Math.random(),
-        color: '#cfcfcf'
-      });
-    }
   }
 
   ngAfterViewInit(): void {
-    new sigma({
-      graph: this.graph,
-      container: this.container.nativeElement.id
+    this.cy = cytoscape({
+      container: this.container.nativeElement,
+      elements: [
+        {data: {id: 'a'}},
+        {data: {id: 'b'}},
+        {data: {id: 'ab', source: 'a', target: 'b'}}
+      ],
+      layout: {
+        name: 'grid'
+      }
     });
   }
 
